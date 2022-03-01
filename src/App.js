4e077036cf2fd5979142ac951bbff65ffcd28786
Christian Go <chrisgo2286@yahoo.com>
2022-02-28 21:28:58 -0500

    wrote functions to determine if tied game or winning move

--- src/App.js
+++ src/App.js
@@ -23,10 +23,17 @@ class App extends Component {
     if(this.invalidMove(id)) {
       return;
     }
+    
     const squares = [...this.state.squares]
-    squares[id-1].content = this.state.currentPlayer;
-    const currentPlayer = this.changePlayer();
+    const currentPlayer = this.state.currentPlayer;
+    squares[id-1].content = currentPlayer;
     this.setState({ currentPlayer, squares })
+
+    if(this.gameOver()) {
+      console.log('Game Over')
+      return;
+    }
+
     this.changePlayer();
   }
 
@@ -35,12 +42,57 @@ class App extends Component {
     return (squares[id-1].content !== '') ? true : false;
   }
 
+  gameOver = () => {
+    if(this.winningMove()) {
+      console.log(this.state.currentPlayer + ' is the winner!')
+      return true;
+    }
+    
+    if(this.gameTied()) {
+      console.log('Tied Game')
+      return true;
+    }
+  }
+
+  winningMove = () => {
+    const winningMoves = [
+      [0, 1, 2], 
+      [3, 4, 5], 
+      [6, 7, 8], 
+      [0, 3, 6], 
+      [1, 4, 7], 
+      [2, 5, 8], 
+      [0, 4, 8], 
+      [2, 4, 6]
+    ]
+    for(var i=0; i<8; i++) {
+      var ndx1 = winningMoves[i][0]
+      var ndx2 = winningMoves[i][1]
+      var ndx3 = winningMoves[i][2]
+
+      if(this.state.squares[ndx1].content === this.state.squares[ndx2].content && this.state.squares[ndx1].content === this.state.squares[ndx3].content && this.state.squares[ndx1].content !== '') {
+        console.log(ndx1, ndx2, ndx3)
+        return true;
+      }
+    }
+  }
+
+  gameTied = () => {
+    for(var i=0; i<9; i++) {
+      if(this.state.squares[i].content === '') {
+        return false;
+      }
+    }  
+    return true;
+  }
   changePlayer = () => {
-    const currentPlayer = this.state.currentPlayer;
+    var currentPlayer = this.state.currentPlayer;
     if(currentPlayer === 'o') {
-      return 'x';
+      currentPlayer = 'x';
+      this.setState({ currentPlayer });
     } else {
-      return 'o';
+      currentPlayer = 'o';
+      this.setState({ currentPlayer });
     }
   }
   
